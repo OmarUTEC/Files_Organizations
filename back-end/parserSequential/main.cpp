302699b059fc1074b3fb61851374068bf6cee2c4
OmarUTEC <javier.chavarria@utec.edu.pe>
2024-09-24 01:43:10 -0500

    parser ;-; basico

---
+++ back-end/parserSequential/main.cpp
@@ -0,0 +1,20 @@
+#include "Parser.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "-- Ejecutando interfaz SQL --\n";
+    string query;
+    Scanner scanner;
+    Parser parser(&scanner);
+    while (true) {
+        cout << ">> ";
+        getline(cin, query);
+        if (query == "exit") break;
+        parser.setInput(query);
+        cout << endl;
+        parser.parse();
+        cout << parser.reportParse() << endl;
+        cout << endl;
+    }
+}
